8444e5b255c392d2e73e5fa961709aa9dc07c178
Aboeleneen <aboeleneen.98@yahoo.com>
2020-04-25 02:07:54 +0200

    fix some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,10 @@
 #include <map>
 using namespace std;
 #include "parser_table.cpp"
-#include "REToNFA.cpp"
-#include "anaylser.cpp"
 int main()
 {
     string filePath;
-    /*cout << "Enter The path of the file containing production rules: "<< endl;
+    cout << "Enter The path of the file containing production rules: "<< endl;
     getline(cin,filePath);
     Parsing_table parser_table(filePath);
     for(auto p:parser_table.productions){
@@ -25,8 +23,7 @@ int main()
             cout << "\n";
         }
         cout << "--------------------\n" <<endl;
-    }*/
-	NFA* nfa = generateNFA(filePath);
+    }
 	
     return 0;
 }
